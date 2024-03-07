3fa8c413f0e8b907b5470555e54adcdda89cfea1
Antriksha Rattan <antriksharattan314@gmail.com>
2024-03-08 01:24:57 +0530

    Binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,27 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+	if (haystack.length === 0){
+		return false
+	}
+
+	let highIdx = haystack.length;
+	let lowIdx = 0;
+
+
+
+	do {
+
+	let midIdx = Math.floor(lowIdx + (highIdx-lowIdx)/2);
+	let midValue = haystack[midIdx]
+
+		if( midValue === needle){
+			return true
+		}else if (needle > midValue){
+			lowIdx = midIdx +1;
+		}else {
+			highIdx = midIdx
+		}	
+	} while(lowIdx < highIdx)
+
+		return false;
 }
