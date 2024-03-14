f544ac9cbe564c35d57613aad588f28dec077802
Antriksha Rattan <antriksharattan314@gmail.com>
2024-03-14 23:48:58 +0530

    Traversal tree

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,25 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[]{
 
+    if(!curr){
+        return path;
+    }
+
+    // 3 step recurse
+
+    // recurse
+    walk(curr.left, path)
+
+    // pre (visiting node)
+    path.push(curr.value)
+    
+    walk(curr.right, path)
+
+    // post
+    return path;
+}
+
+
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head,[])
 }
--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,24 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[]{
 
+    if(!curr){
+        return path;
+    }
+
+    // 3 step recurse
+
+    // recurse
+    walk(curr.left, path)
+    walk(curr.right, path)
+
+    // pre (visiting node)
+     path.push(curr.value)
+
+    // post
+    return path;
+}
+
+
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head,[])
 }
