e44291872689a85b35a2fb515b09110753286b1e
Antriksha Rattan <antriksharattan314@gmail.com>
2024-03-16 00:20:05 +0530

    Compare binary tree

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,17 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    // structural check
+    if(!a && !b){
+        return true
+    }
+
+    // structural check
+    if(!a || !b){
+        return false
+    }
+    //  value check
+    if(a.value !== b.value){
+        return false
+    }
+    return compare(a.left, b.left) && compare(a.right,b.right)
 
 }
