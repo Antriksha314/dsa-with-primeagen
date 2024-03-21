2225fb962e1e29179934166a1ff921178746b0d7
Antriksha Rattan <antriksharattan314@gmail.com>
2024-03-21 23:36:51 +0530

    Setup LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,15 +1,29 @@
+type Node<T>={
+    value:T,
+    next?: Node<T>,
+    prev?: Node<T>
+}
+
+
 export default class LRU<K, V> {
     private length: number;
-
-    
 
     constructor() {
     }
 
     update(key: K, value: V): void {
+        // does it exitss
+        // get(K)
+        // if it doesn't we need to insert
+        //    - check capacity and evict if over
+        //  if it does, we need to update to the front
+        // of the list and update the value
 
-}
+    }
     get(key: K): V | undefined {
 
-}
+        // check the cache for existence
+        // update the value we found and move it to the front
+        // return out the value found or undefiend if not exist
+    }
 }
